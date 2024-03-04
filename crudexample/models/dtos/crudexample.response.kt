d6791d19e1f4b9d37c300895cfba155c65f13326
Sayem Hossain <mail@sayem.dev>
2024-03-04 18:55:42 +0600

    update

--- crudexample/models/dtos/crudexample.response.kt
+++ crudexample/models/dtos/crudexample.response.kt
@@ -4,25 +4,45 @@ import com.example.app.domains.crudexamples.models.entities.CrudExample
 
 data class CrudExampleBriefResponse(
     val id: Long,
+
+    @field:JsonProperty("created_at")
+    val createdAt: Instant,
+
+    @field:JsonProperty("updated_at")
+    val updatedAt: Instant? = null,
+
     val title: String
 )
 
 fun CrudExample.toBriefResponse(): CrudExampleBriefResponse =
     CrudExampleBriefResponse(
         id = this.id,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt,
         title = this.title
     )
 
 data class CrudExampleDetailResponse(
     val id: Long,
+
+    @field:JsonProperty("created_at")
+    val createdAt: Instant,
+
+    @field:JsonProperty("updated_at")
+    val updatedAt: Instant? = null,
+
     val title: String,
+
     val description: String,
+
     val image: String?
 )
 
 fun CrudExample.toDetailResponse(): CrudExampleDetailResponse =
     CrudExampleDetailResponse(
         id = this.id,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt,
         title = this.title,
         description = this.description,
         image = this.image
